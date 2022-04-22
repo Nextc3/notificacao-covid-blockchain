bd15d876167bee12cc4b130454d2f7e4dadd7def
Caio Costa <nextc3@gmail.com>
2022-04-21 21:45:59 -0300

    alteração do nome do pacote

---
+++ interfaceservico/iservico.go
@@ -0,0 +1,11 @@
+package interfaceservico
+
+import (
+	"github.com/Nextc3/notificacao-covid-blockchain/entidade"
+)
+
+type Iservico interface {
+	ObterTodos() ([]*entidade.Notificacao, error)
+	Obter(Id int) (*entidade.Notificacao, error)
+	Salvar(n *entidade.Notificacao) error
+}
